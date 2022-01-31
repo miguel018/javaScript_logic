9fc2710e22f4ffa58451777b6ff4371828ced94f
miguel018 <mouramiguel016@gmail.com>
2022-01-31 22:23:39 +0000

    valor

--- Aula01/handsOn_ex1/js/script.js
+++ Aula01/handsOn_ex1/js/script.js
@@ -1,4 +1,4 @@
 let dolar = parseFloat(prompt("Digite o valor da doação em dolar:")); //O valor em dolar do usuario
-
+let valorDolar = parseFloat(prompt("Digite o valor da cotação do dolar:"));
 //Vai pegar o valor em dolar do usuario e multiplicar pelo valor do real,com 2 casas decimais
-alert(`Isso são ${(dolar * 5.40136).toFixed(2)} R$`);
+alert(`Isso são ${(dolar * valorDolar).toFixed(2)} R$`);
