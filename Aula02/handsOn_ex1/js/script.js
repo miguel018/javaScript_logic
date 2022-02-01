9058f49e1d84c22e3ff8f716b7660f7e0e6ba1c4
miguel018 <mouramiguel016@gmail.com>
2022-02-01 00:37:28 +0000

    aula02

--- Aula02/handsOn_ex1/js/script.js
+++ Aula02/handsOn_ex1/js/script.js
@@ -6,9 +6,12 @@ let media = (nota1 + nota2 + nota3) / 3;
 
 //verifica se usuario tem media para passar ou não
 if (media >= 7) {
+  document.getElementById("final").innerHTML = "Aprovado";
   alert("Aprovado");
 } else if (media < 7 && media >= 4) {
+  document.getElementById("final").innerHTML = "em prova final";
   alert("em prova final");
 } else {
+  document.getElementById("final").innerHTML = "reprovado";
   alert("reprovado");
 }
